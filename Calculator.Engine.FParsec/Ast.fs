f09787de295395633379943fb572836a302e4f42
Benjamin F <deadalus.ai@gmail.com>
2012-04-10 13:34:31 +1200

    Added initial implementation of Engine in FParsec

---
+++ Calculator.Engine.FParsec/Ast.fs
@@ -0,0 +1,14 @@
+﻿module Calculator.FParsec.Ast
+
+type Term =
+| Constant of float
+| Variable of string
+
+and Expr =
+| Term      of Term
+| Add       of Expr * Expr
+| Multiply  of Expr * Expr
+| Subtract  of Expr * Expr
+| Divide    of Expr * Expr
+| Power     of Expr * Expr
+| Root      of Expr * Expr
---
+++ Calculator.Engine.FParsec/Engine.fs
@@ -0,0 +1,34 @@
+﻿module Calculator.FParsec.Engine
+
+open FParsec
+open FParsec.Primitives
+open FParsec.CharParsers
+open Calculator.FParsec.Ast
+
+let private ws = spaces
+let private ws1 = spaces1
+let private str_ws s = pstring s >>. ws
+let private str_ws1 s = pstring s >>. ws1
+
+//matches variable "names" like the following regex: [a-z_][a-z_0-9]*
+let pname =
+    let isAsciiLetterOrUnderscore c = isAsciiLetter c || isAnyOf "_" c
+    let isAsciiLetterOrUnderscoreOrDigit c = isAsciiLetterOrUnderscore c || isDigit c
+    many1Satisfy2 isAsciiLetterOrUnderscore isAsciiLetterOrUnderscoreOrDigit
+
+//parse a float (constant) or variable (name) and convert it to a Term expression 
+let pterm = ((pfloat |>> Constant) <|> (pname |>> Variable)) |>> Term
+
+//implement the pexpr parser
+let pexpr =
+    let opp = new OperatorPrecedenceParser<Expr, unit, unit> ()
+    let expr = opp.ExpressionParser
+    opp.TermParser <- (pterm .>> ws) <|> between (str_ws "(") (str_ws ")") expr
+    //BEDMAS
+    opp.AddOperator(InfixOperator("-", ws, 1, Associativity.Left, fun x y -> Subtract (x, y)))
+    opp.AddOperator(InfixOperator("+", ws, 2, Associativity.Left, fun x y -> Add (x, y)))
+    opp.AddOperator(InfixOperator("*", ws, 3, Associativity.Left, fun x y -> Multiply (x, y)))
+    opp.AddOperator(InfixOperator("/", ws, 4, Associativity.Left, fun x y -> Divide (x, y)))
+    opp.AddOperator(InfixOperator("^", ws, 5, Associativity.Left, fun x y -> Power (x, y)))
+    opp.AddOperator(InfixOperator("r", ws, 6, Associativity.Left, fun x y -> Root (x, y)))
+    expr
