a292ee1c41d28217d77c8330f7589ceb60a14795
Benjamin F <deadalus.ai@gmail.com>
2012-06-23 13:59:41 +1200

    Elimintated the mutable elements of the codebase
    - Removed the IStateHost abstraction
    + Code is now side-effect free?
    + Any failing command reverts the state!

--- Calculator.Console/Program.fs
+++ Calculator.Console/Program.fs
@@ -1,97 +1,97 @@
 ﻿module Calculator.Console.Program
 
-open System
-open System.IO
-open System.Collections.Generic
 open Calculator.Ast
 open Calculator.Implementation
 open Calculator.Console.CommandHelpers
 
-let memory = new Dictionary<string, double> (Map.ofList [ ("pi", 3.14159) ])
-let mutable debug = false
-
-let getVar (key:string) =
-    if not (memory.ContainsKey key) then
-        failwith (sprintf "Variable %s does not exist" key)
-    memory.[key]
-
-let setVar (key:string) (value:double) =
-    match key with
-    | "_" -> printfn "= %g" value
-    | key -> printfn "%s = %g" key value
-    memory.[key] <- value
-
-let clearVar (key:string) =
-    match key with
-    | "_" -> failwith "_ is a protected variable name"
-    | key -> 
-        if not (memory.ContainsKey key) then
-            failwith (sprintf "Variable %s does not exist" key)
-        memory.Remove key |> ignore
-
-let invokeFunction (name:string) (arg:double) =
-    match name with
-    | "sin"  -> Math.Sin(arg)
-    | "cos"  -> Math.Cos(arg)
-    | "tan"  -> Math.Tan(arg)
-    | "sqrt" -> Math.Sqrt(arg)
-    | _ -> failwith (sprintf "Function %s not defined" name)
-
-
-let printVariables () =
-    for key in memory.Keys |> Seq.sortBy (fun key -> key) do
-        if not (key = "_") then
-            printfn "%5s -> %g" key memory.[key]
+let printVariables (state:State) =
+    Map.toSeq state.memory |> 
+    Seq.sortBy (fun (key, value) -> key) |>
+    Seq.iter (fun (key, value) ->
+                if not (key = "_") then
+                    printfn "%5s -> %g" key value)
 
 let printErr message =
     fprintfn System.Console.Error "ERROR: %s" message
 
-let host = { new IStateHost with 
-             member this.SetVariable k v = setVar k v
-             member this.GetVariable k = getVar k
-             member this.ClearVariable k = clearVar k
-             member this.InvokeFunction n a = invokeFunction n a }
+let runEquation (state:State) (line:string) =
+    let parseResult = (parseLine line)
+    match parseResult with
+    | Error msg -> 
+        printErr msg
+        state
+    | Command command -> 
+        if state.debug then printfn "Command: %A" command
+        //executeCommand host command state
+        try
+            let result = executeCommand state command
+            match result with
+            | UpdateResult (newState, assignments) -> 
+                for assignment in assignments do
+                    printfn "%s = %g" assignment.name assignment.value
+                newState
+            | ExpressionResult (newState, value) ->
+                printfn "= %g" value
+                newState
+        with ex ->
+            printErr ex.Message
+            state
 
-let runEquation (line:string) =
+let processFile (fname:string) (state:State) =
     try
-        let parseResult = (parseLine line)
-        match parseResult with
-        | Error msg -> printErr msg
-        | Command command -> 
-            if debug then printfn "Command: %A" command
-            executeCommand host command
+        use reader = new System.IO.StreamReader(System.IO.File.OpenRead fname)
+        let lines = seq {
+            while not reader.EndOfStream do
+                let line = reader.ReadLine().Trim()
+                if line.Length > 0 && not (line.StartsWith "#") then
+                    yield line 
+        }
+        Seq.fold runEquation state lines
     with ex ->
         printErr ex.Message
-    
-let runFile (fname:string) =
-    try
-        use reader = new StreamReader (File.OpenRead fname)
-        while not reader.EndOfStream do
-            let line = reader.ReadLine().Trim()
-            if line.Length > 0 && not (line.StartsWith "#") then
-                runEquation line
-    with ex ->
-        printErr ex.Message
+        state
+
+let processCommand (state:State) (command:Command) =
+    match command with
+    | Exit -> exit 0
+    | Help -> 
+        Help.write ()
+        state
+    | ClearScreen -> 
+        try 
+            System.Console.Clear()
+            state
+        with ex -> state
+    | ReadFile fname -> 
+        processFile fname state
+    | PrintVariables -> 
+        printVariables state
+        state
+    | Debug arg ->
+        match arg with
+        | "on"  -> { state with debug = true }
+        | "off" -> { state with debug = false }
+        | null  -> 
+            printfn "Debug mode %s" (if state.debug then "enabled" else "disabled")
+            state
+        | _     -> 
+            printfn "Invalid argument: %s" arg
+            state
+    | EquationCommand equation -> 
+        runEquation state equation
 
 //start the main loop
 printfn "Calculator - type '?' for help, 'q' to quit"
-while true do
-    printf "> "
-    match Console.ReadLine().Trim() with
-    //no input - continue
-    | "" -> ()
-    //a calculator command - process
-    | commandString ->
-        match (Command.create commandString) with
-        | Exit -> exit 0
-        | Help -> Help.write ()
-        | ClearScreen -> try Console.Clear() with ex -> ()
-        | ReadFile fname -> runFile fname
-        | PrintVariables -> printVariables ()
-        | Debug arg ->
-            match arg with
-            | "on"  -> debug <- true
-            | "off" -> debug <- false
-            | null  -> printfn "Debug mode %s" (if debug then "enabled" else "disabled")
-            | _     -> printfn "Invalid argument: %s" arg
-        | EquationCommand equation -> runEquation equation
+let commands = seq { 
+    while true do
+        printf "> "
+        match System.Console.ReadLine().Trim() with
+        | "" -> ()
+        | commandString -> yield Command.create commandString 
+}
+
+let initialState = { memory = Map.ofList [ ("pi", 3.14159) ];
+                     debug = false }
+
+//start the main loop
+Seq.fold processCommand initialState commands |> ignore
--- Calculator.Engine.FParsec/Implementation.fs
+++ Calculator.Engine.FParsec/Implementation.fs
@@ -1,74 +1,92 @@
 ﻿module Calculator.Implementation
 
-open System
-open System.Collections.Generic
-
 open FParsec.Primitives
 open FParsec.CharParsers
 
 open Calculator.Ast
 open Calculator.Engine
 
-type IStateHost =
-    interface
-       abstract member GetVariable : key:string -> double
-       abstract member SetVariable : key:string -> double -> unit
-       abstract member ClearVariable : key:string -> unit
-       abstract member InvokeFunction : name:string -> double -> double
-    end
+type State = { memory : Map<string, float>; debug: bool }
 
 type ParseResult =
 | Command of Command
 | Error of string
 
-let parseLine (line:string) =
+type AssignmentResult = { name : string; value : float }
+
+type CommandResult = 
+| ExpressionResult of State * float
+| UpdateResult of State * AssignmentResult list
+
+let parseLine line =
     let parserResult = runParserOnString pcommand_eof () "Input" line
     match parserResult with
     | Success (expr, state, pos) -> Command expr
     | Failure (msg, err, state) -> Error msg
         
-let rec evalExpr (s:IStateHost) (expr:Expr) =
+let rec evalExpr state expr =
+    let evalExpr' = evalExpr state
     match expr with
-    | Term t -> evalTerm s t
-    | Add (l, r) -> (evalExpr s l) + (evalExpr s r)
-    | Multiply (l, r) -> (evalExpr s l) * (evalExpr s r)
-    | Subtract (l, r) -> (evalExpr s l) - (evalExpr s r)
-    | Divide (l, r) -> (evalExpr s l) / (evalExpr s r)
-    | Power (l, r) -> Math.Pow(evalExpr s l, evalExpr s r)
-    | Modulo (l, r) -> (evalExpr s l) % (evalExpr s r)
-    | Negative e -> -1.0 * (evalExpr s e)
-    | FunctionCall (name, e) -> evalFunction s name e
+    | FunctionCall (name, e) -> evalFunction state name e
+    | Term t -> evalTerm state t
+    | Add (l, r) -> (evalExpr' l) + (evalExpr' r)
+    | Multiply (l, r) -> (evalExpr' l) * (evalExpr' r)
+    | Subtract (l, r) -> (evalExpr' l) - (evalExpr' r)
+    | Divide (l, r) -> (evalExpr' l) / (evalExpr' r)
+    | Power (l, r) -> System.Math.Pow(evalExpr' l, evalExpr' r)
+    | Modulo (l, r) -> (evalExpr' l) % (evalExpr' r)
+    | Negative e -> -1.0 * (evalExpr' e)
         
-and evalFunction (s:IStateHost) (name:Name) (e:Expr) =
-    s.InvokeFunction (evalName name) (evalExpr s e)
+and evalFunction state name expr =
+    let name = (evalName name)
+    let arg = (evalExpr state expr)
+    match name with
+    | "sin"  -> System.Math.Sin(arg)
+    | "cos"  -> System.Math.Cos(arg)
+    | "tan"  -> System.Math.Tan(arg)
+    | "sqrt" -> System.Math.Sqrt(arg)
+    | _ -> failwith (sprintf "Function %s not defined" name)
 
-and evalTerm (s:IStateHost) (term:Term) =
+and evalTerm state term =
     match term with
     | Constant c -> c
-    | Variable n -> evalVariable s n
+    | Variable n -> evalVariable state n
 
-and evalName (name:Name) = match name with Name n -> n
+and evalName name = match name with Name n -> n
 
-and evalVariable (s:IStateHost) (name:Name) =
-    s.GetVariable(evalName name)
-    
+and evalVariable state name =
+    let key = (evalName name)
+    match (Map.tryFind key state.memory) with
+    | Some v -> v
+    | None -> failwith (sprintf "Variable %s does not exist" key)
+
 /// Execute a command
-let executeCommand (s:IStateHost) (eq:Command) =
-    match eq with
-    | Expr expr -> 
-        let result = (evalExpr s expr)
-        s.SetVariable "_" result
+let executeCommand state command =
+    let setMem state key value = { state with memory = Map.add key value state.memory }
+    let clearMem state key = { state with memory = Map.remove key state.memory }
+    
+    match command with
+    | Expr expr ->
+        let result = (evalExpr state expr)
+        let newState = setMem state "_" result
+        ExpressionResult (newState, result)
         
     | Update list ->
-        //iterate through each update command, apply it and yield a result
-        for command in list do
-            match command with
+        //Each update may optionally add to the list of "assignment results"
+        let applyUpdate (state, results) update =
+            match update with
             | Assignment (name, expr) -> 
                 let name = (evalName name)
-                let result = (evalExpr s expr)
-                s.SetVariable name result
+                let result = (evalExpr state expr)
+                let newState = setMem state name result
+                (newState, { name = name; value = result } :: results)
 
             | Deletion (name) ->
-                (evalVariable s name) |> ignore //checks to make sure the variable exists
+                (evalVariable state name) |> ignore //checks to make sure the variable exists
                 let name = (evalName name)
-                s.ClearVariable name
+                let newState = clearMem state name
+                (newState, results)
+        
+        //fold the list of "updates" to generate a new state and a list of assignments
+        let newState, results = Seq.fold applyUpdate (state, []) list
+        UpdateResult (newState, List.rev results)
